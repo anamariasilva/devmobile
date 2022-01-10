94f067e04960dbae1acb05f1a57d5d0533e018a8
Ana Maria <developer@anamariasilva.com.br>
2022-01-10 08:23:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Comandos git no GitHub.");
+
+/*
+echo "# devmobile" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/anamariasilva/devmobile.git
+git push -u origin main
+
+git remote add origin https://github.com/anamariasilva/devmobile.git
+git branch -M main
+git push -u origin main
+
+*/
